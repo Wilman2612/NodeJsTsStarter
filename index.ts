db60e829a846c0160c1390851d6cb1f58ab6fec6
Wilman <wilman2612@gmail.com>
2021-01-06 01:40:01 -0500

    mongo+ docker network

--- index.ts
+++ index.ts
@@ -1,7 +1,29 @@
 import * as express from 'express'
+import * as mongoose from 'mongoose'
+require('dotenv').config()
 
 var app=express()
 
-app.get("/",(_req, res)=>{ res.send("hello world!!!")})
+// configuramos la app para que use bodyParser(), esto nos dejara usar la informacion de los POST
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.listen(3000, ()=>{ console.log("listening at port 3000")})
+const port = process.env.PORT || 3000;        // seteamos el puerto
+const dbCnn = process.env.MONGO ||  "mongodb://localhost:27017/test"
+
+app.get("/",(_req, res)=>{ 
+    mongoose.connect(dbCnn, {useNewUrlParser:true},(err)=>{
+        if(err){
+            res.status(500).send("Error de conexión a la bd: "+ err)
+        }else{
+            res.send("hello world!!!")
+        }
+    })
+    
+})
+
+app.listen(port, ()=>{ console.log("listening at port 3000")})
+
+process.on('SIGINT', function() {
+    process.exit();
+});
